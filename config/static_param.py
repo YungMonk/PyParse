25735513b71af5b43fb2138de10f207993e7cafb
Yung <xin.yang@ifchange.com>
2020-06-12 15:15:10 +0800

    渠道配置添加

--- config/static_param.py
+++ config/static_param.py
@@ -1,7 +1,62 @@
 channelMap = {
-    1: "zhaopin",
-    2: "51job",
-    51: "carjob"
+    1: "zhaopin",     # 智联
+    2: "51job",     # 51job
+    3: "liepin",     # 猎聘
+    4: "dajie",     # 大街
+    7: "cjol",     # 中国人才热线
+    9: "chinahr",     # 中华英才网
+    11: "lagou",     # 拉勾
+    12: "58",     # 58同城
+    13: "ganji",     # 赶集
+    20: "linkedin",     # 领英
+    21: "51jrjob",     # 中国金融人才网
+    22: "goodjobs",     # 新安人才网
+    23: "rc114",     # 成都人才网
+    24: "pinbot",     # 聘宝
+    25: "zhaoluobo",     # 找萝卜
+    26: "rencaiaaa",     # 人才啊
+    27: "kanzhun",     # 看准网
+    28: "juzi",     # 桔子网
+    30: "lieshang",     # 猎上网
+    31: "zhuopin",     # 卓聘
+    32: "kshr",     # 昆山人才网
+    33: "zhipin",     # Boss直聘
+    34: "buildhr",     # 建筑英才网
+    35: "jianlika",     # 简历咖
+    36: "miaohr",     # 妙招网
+    37: "fenjianli",     # 纷简历
+    38: "maimai",     # 脉脉
+    39: "wiki",     # 维基百科
+    40: "weibo",     # 新浪微博
+    41: "job592",     # 职友圈
+    42: "tianyancha",     # 天眼查
+    43: "lieluobo",     # 猎萝卜
+    44: "jobcn",     # 卓博
+    45: "shixiseng",     # 实习僧
+    46: "huibo",     # 汇博
+    47: "0577hr",     # 温州人力资源网
+    48: "cfw",     # 服装人才网
+    49: "xumurc",     # 牧通人才网
+    51: "carjob",     # 汽车人才网
+    52: "doctorjob",     # 中国医疗人才网
+    53: "bjx",     # 北极星
+    54: "51jingying",     # 无忧精英
+    55: "doumi",     # 斗米
+    56: "baixing",     # 百姓网
+    57: "dianzhang",     # 店长直聘
+    58: "youlanw",     # 优蓝网
+    59: "dingxiangyuan",     # 丁香园
+    60: "xmrc",     # 厦门人才网
+    10001: "nacao",     # 全国统一社会信用代码信息核查系统
+    10002: "zhihu",     # 知乎
+    10003: "dingxiangyuan",     # 丁香园
+    10009: "gxrc",     # 广西人才网
+    10010: "597",     # 597人才网
+    10011: "qzrc",     # 泉州人才网
+    10012: "shoeshr",     # 中国鞋业人才网
+    10017: "o-hr",     # 圆才网
+    10021: "veryeast",     # 最佳东方
+    10022: "jianzhimao",     # 兼职猫
 }
 
 parserType = {
