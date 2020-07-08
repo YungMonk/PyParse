925a83437430d052cf3b05dd9332612b8d636169
Yung <xin.yang@ifchange.com>
2020-07-08 14:30:54 +0800

    人才热线

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ from utils import strings
 
 
 if __name__ == '__main__':
-    filename = os.getcwd() + '/test/data/resume/cfw/zh1_1.html'
+    filename = os.getcwd() + '/test/data/resume/cjol/mail_1.html'
     with open(filename, 'r') as f:
         fileContext = f.read()
         f.close()
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         'c': 'apis/logic_parse',
         'm': 'parsers_engine',
         'p': {
-            'site_id': 48,
+            'site_id': 7,
             'type': 1,
             'body': fileContext,
         }
